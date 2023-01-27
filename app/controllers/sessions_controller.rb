581f634975e9d44e456f8f52a9c62014c83e7d9a
Sean Martin <sean@seanmartinstudio.com>
2023-01-27 14:28:53 -0800

    adds sessions_controller actions and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+    #LoginForm end point
+    #POST '/login'
+    def create 
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
+    #LogoutForm end point
+    #DElETE '/logout'
+    def destroy 
+        user = User.find_by(username: params[:username])
+        if session[:user_id]
+            session.delete :user_id
+            head :no_content
+        else  
+            render json: { errors: ["Not Authorized"] }, status: :unauthorized
+        end
+    end
 end
