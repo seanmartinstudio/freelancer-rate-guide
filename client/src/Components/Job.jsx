7e0443cc8b4db4be4a788e6a309b04ba66499f4c
Sean Martin <sean@seanmartinstudio.com>
2023-02-10 15:40:12 -0800

    starter UI for Home Page

--- client/src/Components/Job.jsx
+++ client/src/Components/Job.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Job = () => {
+const Job = ( {job, company, industry} ) => {
+  const {job_title, job_description, rate} = job
+
   return (
-    <artcile>
-        <h3>Post Here...</h3>
-    </artcile>
+  <article>
+    <p>{job_title}</p>
+    <p>{job_description}</p>
+    <p>${rate}/hour</p>
+    <p>{company}</p>
+    <p>{industry}</p>
+  </article>
   )
 }
 
--- client/src/Components/JobsContainer.jsx
+++ client/src/Components/JobsContainer.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Job from './Job'
 import JobSearchBar from './JobSearchBar'
+import axios from 'axios'
 
 const JobsContainer = () => {
+    const[jobs, setJobs] = useState([])
+  
+  useEffect(() => {
+    axios.get('/jobs')
+  .then(function (response) {
+   setJobs(response.data)
+  })
+  .catch(function (error) {
+    console.log("Jobs Error:", error)
+  })
+  }, []);
+
+  
+  console.log("JOBS OBJ:", jobs)
+
   return (
     <section>
         <JobSearchBar/>
-        <article>
-            {/* <Job/> */}
-            <p>blaahahah</p>
-            <p>blaahahah</p>
-            <p>blaahahah</p>
-            <p>blaahahah</p>
-        </article>
+    
+        {jobs.map((job) => {
+        return (
+        <Job job={job} key={job.id} company={job.company.company_size} industry={job.industry.industry_type}/> )
+      })}
+    
     </section>
    
   )
