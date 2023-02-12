33be065099e3d965692e116a3d5a5a7c4e02a770
Sean Martin <sean@seanmartinstudio.com>
2023-02-12 11:31:07 -0800

    seed Job data for Render

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 
  
-    ### USER SEED DATA
-    User.create!(username: "Sean Martin", password: "Los Angeles", email: "sean@seanmartinstudio.com")
-    User.create!(username: "Charles Boyce", password: "Cambridge", email: "charles.boyce@gmail.com")
+#     ### USER SEED DATA
+#     User.create!(username: "Sean Martin", password: "Los Angeles", email: "sean@seanmartinstudio.com")
+#     User.create!(username: "Charles Boyce", password: "Cambridge", email: "charles.boyce@gmail.com")
 
 
-    # ### COMPANY SEED DATA
-    Company.create!(company_size: "Micro (0 to 9 employees)")
-    Company.create!(company_size: "Small (10 to 99 employees)")
-    Company.create!(company_size: "Mid-sized (100 to 999 employees)")
-    Company.create!(company_size: "Large (1,000 or more employees)")
-    Company.create!(company_size: "Very large (5,000 or more employees)")
+#     # ### COMPANY SEED DATA
+#     Company.create!(company_size: "Micro (0 to 9 employees)")
+#     Company.create!(company_size: "Small (10 to 99 employees)")
+#     Company.create!(company_size: "Mid-sized (100 to 999 employees)")
+#     Company.create!(company_size: "Large (1,000 or more employees)")
+#     Company.create!(company_size: "Very large (5,000 or more employees)")
 
-    # ### INDUSTRY SEED DATA
-    Industry.create!(industry_type: "Advertising")
-    Industry.create!(industry_type: "Marketing")
-    Industry.create!(industry_type: "Publishing")
-    Industry.create!(industry_type: "Film and Television")
-    Industry.create!(industry_type: "Music")
-    Industry.create!(industry_type: "Fashion and Retail")
-    Industry.create!(industry_type: "Architecture and Interior Design")
-    Industry.create!(industry_type: "Manufacturing and Construction")
-    Industry.create!(industry_type: "Healthcare")
-    Industry.create!(industry_type: "Legal")
-    Industry.create!(industry_type: "Consulting")
+#     # ### INDUSTRY SEED DATA
+#     Industry.create!(industry_type: "Advertising")
+#     Industry.create!(industry_type: "Marketing")
+#     Industry.create!(industry_type: "Publishing")
+#     Industry.create!(industry_type: "Film and Television")
+#     Industry.create!(industry_type: "Music")
+#     Industry.create!(industry_type: "Fashion and Retail")
+#     Industry.create!(industry_type: "Architecture and Interior Design")
+#     Industry.create!(industry_type: "Manufacturing and Construction")
+#     Industry.create!(industry_type: "Healthcare")
+#     Industry.create!(industry_type: "Legal")
+#     Industry.create!(industry_type: "Consulting")
 
     # # ### JOB SEED DATA
-    # Job.create!(job_title: "Graphic Design", job_description: "Commissioned by the Art Director to create a series of movie posters for the Los Angeles area.", rate: 100.00, user: sean, industry_id: 4, company_id: 5, experience: "Senior")
-    # Job.create!(job_title: "Graphic Design", job_description: "Created an invite for a company party", rate: 50.00, user: charles, industry_id: 9, company_id: 2, experience: "Junior")
+    Job.create!(job_title: "Art Director", job_description: "Commissioned by the Creative Director to create a series of movie posters for the Los Angeles area.", rate: 100.00, user_id: 1, industry_id: 4, company_id: 5, experience: "Senior Level")
+    Job.create!(job_title: "Developer", job_description: "Build e-commerce wite", rate: 200.00, user_id: 2, industry_id: 6, company_id: 2, experience: "Mid Level")
