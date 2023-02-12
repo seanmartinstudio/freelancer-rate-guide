d7b49d96f15cd1314c31559f02f74d499614600b
Sean Martin <sean@seanmartinstudio.com>
2023-02-12 09:47:47 -0800

    adds build.sh file

---
+++ bin/freelance-rate-build.sh
@@ -0,0 +1,7 @@
+set -o errexit
+
+bundle install
+# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+# bundle exec rake assets:clean
+bundle exec rake db:migrate 
+bundle exec rake db:seed
