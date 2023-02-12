aefc57b3cdd485eb592739abc5132324892c00f5
Sean Martin <sean@seanmartinstudio.com>
2023-02-12 11:22:18 -0800

    add User Company Industry seed data to Render

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,29 @@
 
  
     ### USER SEED DATA
-    # sean = User.create!(username: "Sean Martin", password: "Los Angeles", email: "sean@seanmartinstudio.com")
-    # charles = User.create!(username: "Charles Boyce", password: "Cambridge", email: "Charlies.Boyce@gmail.com")
+    User.create!(username: "Sean Martin", password: "Los Angeles", email: "sean@seanmartinstudio.com")
+    User.create!(username: "Charles Boyce", password: "Cambridge", email: "charles.boyce@gmail.com")
 
 
     # ### COMPANY SEED DATA
-    # Company.create!(company_size: "Micro (0 to 9 employees)" )
-    # Company.create!(company_size: "Small (10 to 99 employees)")
-    # Company.create!(company_size: "Mid-sized (100 to 999 employees)")
-    # Company.create!(company_size: "Large (1,000 or more employees)")
-    # Company.create!(company_size: "Very large (5,000 or more employees)" )
+    Company.create!(company_size: "Micro (0 to 9 employees)")
+    Company.create!(company_size: "Small (10 to 99 employees)")
+    Company.create!(company_size: "Mid-sized (100 to 999 employees)")
+    Company.create!(company_size: "Large (1,000 or more employees)")
+    Company.create!(company_size: "Very large (5,000 or more employees)")
 
     # ### INDUSTRY SEED DATA
-    # Industry.create!(industry_type: "Advertising")
-    # Industry.create!(industry_type: "Marketing")
-    # Industry.create!(industry_type: "Publishing")
-    # Industry.create!(industry_type: "Film and Television")
-    # Industry.create!(industry_type: "Music")
-    # Industry.create!(industry_type: "Fashion and Retail")
-    # Industry.create!(industry_type: "Architecture and Interior Design")
-    # Industry.create!(industry_type: "Manufacturing and Construction")
-    # Industry.create!(industry_type: "Healthcare")
-    # Industry.create!(industry_type: "Legal")
-    # Industry.create!(industry_type: "Consulting")
+    Industry.create!(industry_type: "Advertising")
+    Industry.create!(industry_type: "Marketing")
+    Industry.create!(industry_type: "Publishing")
+    Industry.create!(industry_type: "Film and Television")
+    Industry.create!(industry_type: "Music")
+    Industry.create!(industry_type: "Fashion and Retail")
+    Industry.create!(industry_type: "Architecture and Interior Design")
+    Industry.create!(industry_type: "Manufacturing and Construction")
+    Industry.create!(industry_type: "Healthcare")
+    Industry.create!(industry_type: "Legal")
+    Industry.create!(industry_type: "Consulting")
 
     # # ### JOB SEED DATA
     # Job.create!(job_title: "Graphic Design", job_description: "Commissioned by the Art Director to create a series of movie posters for the Los Angeles area.", rate: 100.00, user: sean, industry_id: 4, company_id: 5, experience: "Senior")
