7c6bcfa4384641d6fdda04fc069926aae3e69399
Sean Martin <sean@seanmartinstudio.com>
2023-02-12 11:05:49 -0800

    update build sh file for front end?

--- bin/project-build.sh
+++ bin/project-build.sh
@@ -2,8 +2,12 @@
 # exit on error
 set -o errexit
 
+# builds the front end code
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# builds the back end code
 bundle install
-# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
-# bundle exec rake assets:clean
-bundle exec rake db:migrate 
-bundle exec rake db:seed
+bundle exec rake db:migrate
+bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
