632e2011f9422f2ae109b6039dd500b9f207de16
Sean Martin <sean@seanmartinstudio.com>
2023-02-14 20:57:30 -0800

    adds Edit and Delete buttons to UserJob" git push git commit -m "adds Edit and Delete buttons to UserJob" exit " git push

--- client/src/Components/UserJob.jsx
+++ client/src/Components/UserJob.jsx
@@ -4,15 +4,21 @@ const UserJob = ( {job, company, industry, user} ) => {
   const {job_title, job_description, rate, experience} = job
 
   return (
-  <article>
-    <p>{job_title}</p>
-    <p>{job_description}</p>
-    <p>${rate}/hour</p>
-    <p>{experience}</p>
-    <p>{company}</p>
-    <p>{industry}</p>
-    <p>{user}</p>
-  </article>
+  <div>
+    <div className='sideButtonWrap'>
+    <button class="side-button">Edit</button>
+    <button class="side-button">Delete</button>
+    </div>
+    <article>
+      <p>{job_title}</p>
+      <p>{job_description}</p>
+      <p>${rate}/hour</p>
+      <p>{experience}</p>
+      <p>{company}</p>
+      <p>{industry}</p>
+      <p>{user}</p>
+    </article>
+  </div>
   )
 }
 
