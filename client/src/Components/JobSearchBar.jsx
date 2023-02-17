ac84aa0e928890d5fc13c84d5fefccd5a72c402c
Sean Martin <sean@seanmartinstudio.com>
2023-02-17 14:39:37 -0800

    creates drop down options for JobSearchBar

--- client/src/Components/JobSearchBar.jsx
+++ client/src/Components/JobSearchBar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import axios from 'axios'
 
 const JobSearchBar = () => {
+  const [jobTitle, setJobTitle] = useState("")
+
+  const jobTitleArr = ["Animator", "Art Director", "Cinematographer", "Copy Editor", "Copywriter", "Creative Director", "Developer", "Graphic Designer", "Illustrator", "Motion Graphics Designer", "Photographer", "Producer", "Product Designer", "Project Manager", "Strategist", "UX/UI Designer", "Video Director", "Video Editor"]
+
+  const jobTitleOptions = jobTitleArr.map((job) =>
+        <option>{job}</option>
+        )
+
+    console.log("Drop Down Job Title:", jobTitle)
+
   return (
-   <select>
-        <option>Graphic Design</option>
-        <option>Web Development</option>
-   </select>
+    <form>
+       <select name="jobtitle" id="jobtitle" value={jobTitle} onChange={(event) => setJobTitle(event.target.value)} >
+        <option id="jobtitles" value="">Job Title...</option>
+        {jobTitleOptions}
+        </select>
+   </form>
   )
 }
 
