b3330840028392518c3e35eb527446889fea6944
Sean Martin <sean@seanmartinstudio.com>
2023-02-19 17:47:54 -0800

    adds active nav color css to buttons

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,14 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from '../App'
 
-
-
-
-
-
 const NavBar = () => {
 
+  const [activePage, setActivePage] = useState('home')
   const navigate = useNavigate()
   const [user, setUser] = useContext(UserContext)
 
@@ -19,17 +15,23 @@ const NavBar = () => {
         }}).then(navigate('/'))
     }
 
+    function handleNavigationClick(page) {
+      setActivePage(page)
+    }
+
   return (
     <nav className='nav'>
-      
         <Link to="/">
-        <button className='button' type='button'>All Job Posts</button>
+        <button  type='button' className={activePage === 'home' ? 'active' : ''}
+            onClick={() => handleNavigationClick('home')}>All Job Posts</button>
         </Link>
         <Link to="/create-job-post">
-        <button className='button' type='button'>Create Job Post</button>
+        <button type='button'  className={activePage === 'create-job-post' ? 'active' : ''}
+            onClick={() => handleNavigationClick('create-job-post')}>Create Job Post</button>
         </Link>
         <Link to="/user-job-posts">
-        <button className='button' type='button'>User Job Posts</button>
+        <button  type='button'  className={activePage === 'user-job-posts' ? 'active' : ''}
+            onClick={() => handleNavigationClick('user-job-posts')}>User Job Posts</button>
         </Link>
         <button className="button" type="button" onClick={handleLogoutClick}>Log Out</button>
     </nav>
