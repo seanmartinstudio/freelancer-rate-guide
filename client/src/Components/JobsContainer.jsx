315259122bfa10eaec047d8c510369649388e4a4
Sean Martin <sean@seanmartinstudio.com>
2023-02-20 18:40:05 -0800

    updates filter method in Jobs controller

--- client/src/Components/JobsContainer.jsx
+++ client/src/Components/JobsContainer.jsx
@@ -4,18 +4,19 @@ import JobSearchBar from './JobSearchBar'
 import axios from 'axios'
 
 const JobsContainer = () => {
-    const [jobTitle, setJobTitle] = useState("All")
+    const [jobTitle, setJobTitle] = useState("")
+    const [rate, setRate] = useState(500)
     const[jobs, setJobs] = useState([])
   
   useEffect(() => {
-    axios.get('/jobs_search/' + jobTitle)
+    axios.get('/jobs/filter/' + jobTitle + '/' + parseInt(rate))
   .then(function (response) {
    setJobs(response.data)
   })
   .catch(function (error) {
     console.log("Jobs Error:", error)
   })
-  }, [jobTitle]);
+  }, [jobTitle, rate]);
 
 
   const jobTitleArr = ["Animator", "Art Director", "Cinematographer", "Copy Editor", "Copywriter", "Creative Director", "Developer", "Graphic Designer", "Illustrator", "Motion Graphics Designer", "Photographer", "Producer", "Product Designer", "Project Manager", "Strategist", "Video Director", "Video Editor"]
@@ -24,7 +25,14 @@ const JobsContainer = () => {
         <option>{job}</option>
         )
 
+  const rateArr = [50, 100, 200, 300, 400, 500]
+
+  const rateOptions = rateArr.map((rate) =>
+  <option>{rate}</option>
+  )
+
     console.log("Drop Down Job Title:", jobTitle)
+    console.log("Drop Down Rate:", parseInt(rate))
     console.log("Fetch for Job by title", jobs)
 
   return (
@@ -32,8 +40,13 @@ const JobsContainer = () => {
     <section>
         <form>
        <select name="jobtitle" id="jobtitle" value={jobTitle} onChange={(event) => setJobTitle(event.target.value)} >
-        <option id="jobtitles" value="All">All Job Titles</option>
+        {/* <option id="jobtitles" value="All">All Job Titles</option> */}
         {jobTitleOptions}
+        </select>
+
+        <select name="rate" id="rate" value={rate} onChange={(event) => setRate(event.target.value)} >
+        {/* <option id="rate" value="All">All Job Titles</option> */}
+        {rateOptions}
         </select>
    </form>
     
