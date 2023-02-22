a2bb9fdb6a4c025c33bbdba1ea093d5bc9effb96
Sean Martin <sean@seanmartinstudio.com>
2023-02-22 07:52:04 -0800

    fixes bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
     has_many :companies, through: :jobs
     has_many :industries, through: :jobs
     has_secure_password
-    validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
-    validates :password, presence: true, length: { in: 8..64 }
-    validates :email, presence: true, uniqueness: true, :email_format => { :message => "not valid" }
+    # validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
+    # validates :password, presence: true, length: { in: 8..64 }
+    # validates :email, presence: true, uniqueness: true, :email_format => { :message => "not valid" }
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :email, presence: true
 end
