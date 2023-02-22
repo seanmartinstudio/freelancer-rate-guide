82552f3de27648ce6fce4c31a9b09e007321a51a
Sean Martin <sean@seanmartinstudio.com>
2023-02-22 09:18:18 -0800

    fix model typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     # validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
     # validates :password, presence: true, length: { in: 8..64 }
     # validates :email, presence: true, uniqueness: true, :email_format => { :message => "not valid" }
-    validates :username, presence: true, uniqueness: true,
+    validates :username, presence: true, uniqueness: true
     validates :password, presence: true
     validates :email, presence: true
 end
